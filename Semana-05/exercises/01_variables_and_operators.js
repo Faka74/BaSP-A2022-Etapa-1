8c6c22ebd3f8b2ef50e221c64f70b3b4d5961c1a
Facundo Taddia <facundotaddia35@gmail.com>
2022-09-20 02:20:32 -0300

    BaSP-W05-2022:Ex-01-Variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+// Exercise-1a
+var numeroUno = 15;
+var numeroDos = 5;
+var sumados = numeroUno + numeroDos;
+console.log(sumados);
+
+// Exercise-1b
+var oracion = ('Mi nombre es ');
+var nombre = ('Facundo Taddia');
+var oracionCompleta = (oracion + nombre)
+console.log(oracionCompleta);
+
+// Exercise-1c
+var palabra = ('Estas palabras');
+var medidas = ('Seran medidas');
+
+console.log(palabra.length);
+console.log(medidas.length);
+
+var resultadoMedicion = (palabra + medidas)
+console.log(resultadoMedicion.length);
