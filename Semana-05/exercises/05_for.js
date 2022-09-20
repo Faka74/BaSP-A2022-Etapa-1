e0a1e2e450fb0fdf039b401faea3ba00868e4650
Facundo Taddia <facundotaddia35@gmail.com>
2022-09-20 20:29:25 -0300

    BaSP-W05-2022:Ex-05-for

---
+++ Semana-05/exercises/05_for.js
@@ -0,0 +1,20 @@
+// Exercise-5a
+var palabras = ["palabraUno", "palabraDos", "palabraTres", "palabraCuatro", "palabraCinco"]
+console.log(palabras);
+
+for (i = 0; i <= palabras.length - 1; i++)
+
+    // Exercise-5b
+    console.log(alert(palabras[i].substring(0, 1).toUpperCase() + palabras[i].substring(1, 13)));
+
+// Exercise-5c
+var sentence = ('');
+for (i = 0; i <= palabras.length - 5; i++)
+    console.log(alert(palabras));
+
+// Exercise-5d
+var array = [];
+for (var i = 0; i < 10; i++) {
+    array.push(i)
+}
+console.log(array);
